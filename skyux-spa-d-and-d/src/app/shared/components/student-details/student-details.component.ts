83e434e5ed5e63d69294c96914a813d68a9886a2
Bobby Earl <bobby.earl@blackbaud.com>
2019-02-23 15:06:35 -0500

    Getting student details

--- skyux-spa-d-and-d/src/app/shared/components/student-details/student-details.component.ts
+++ skyux-spa-d-and-d/src/app/shared/components/student-details/student-details.component.ts
@@ -1,23 +1,42 @@
 import {
   Component,
-  Input
+  Input,
+  OnInit
 } from '@angular/core';
+
+import { Student } from '../../models';
+import { StudentService } from '../../services';
 
 @Component({
   selector: 'app-student-details',
   templateUrl: './student-details.component.html',
   styleUrls: ['./student-details.component.scss']
 })
-export class StudentDetailsComponent {
+export class StudentDetailsComponent implements OnInit {
 
   public gameStarted = false;
 
   @Input()
   public sid: string;
 
+  public student: Student;
+
+  constructor (
+    private studentService: StudentService
+  ) {}
+
   public startGame() {
     console.log('Starting the game');
     this.gameStarted = true;
+  }
+
+  public ngOnInit() {
+    this.studentService
+      .get(this.sid)
+      .subscribe((student: Student) => {
+        this.student = student;
+        console.log(student);
+      });
   }
 
 }
