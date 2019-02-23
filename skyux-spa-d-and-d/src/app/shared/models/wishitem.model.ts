65954a741e9bdd7bb64fa6efc88b4137b35d0892
Bobby Earl <bobby.earl@blackbaud.com>
2019-02-23 16:15:29 -0500

    Fixed wishlist model

--- skyux-spa-d-and-d/src/app/shared/models/wishitem.model.ts
+++ skyux-spa-d-and-d/src/app/shared/models/wishitem.model.ts
@@ -1,8 +1,14 @@
 export interface WishItem {
   num: number;
   name: string;
-  price_num: number;
-  price_str: string;
-  picture_url: string;
   comment: string;
+  link: string;
+  'old-price': string;
+  'new-price': string;
+  'date-added': string;
+  priority: string;
+  rating: string;
+  'total-ratings': string;
+  picture: string;
+  page: number;
 }
