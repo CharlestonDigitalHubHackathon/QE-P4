b0de2d420eca355242d7c560707b758fd363c16f
Christi Schneider <christineviets@gmail.com>
2019-02-23 14:52:16 -0500

    Experimental lab

--- skyux-spa-d-and-d/src/app/shared/components/game-step/game-step.component.ts
+++ skyux-spa-d-and-d/src/app/shared/components/game-step/game-step.component.ts
@@ -1,6 +1,10 @@
 import {
-  Component, ViewChild, OnInit, ElementRef
+  Component,
+  // ViewChild,
+  OnInit,
+  // ElementRef
 } from '@angular/core';
+import { Character } from '../../models';
 
 @Component({
   selector: 'app-game-step',
@@ -9,24 +13,39 @@ import {
 })
 export class GameStepComponent implements OnInit {
 
+  private character: Character = {
+    name: 'Bobby',
+    literate: false
+  };
+
+  private SHIFT_NUM = Math.floor(Math.random() * Math.floor(26));
+
   public choices = [
     {
-      icon: 'filter',
-      name: 'Exit'
+      id: 1,
+      icon: 'arrow-circle-o-left',
+      name: this.character.literate ? 'Exit' : this.caesarShift('Exit', this.SHIFT_NUM),
+      description: 'Door 1'
+    },
+    {
+      id: 2,
+      icon: 'arrow-circle-o-right',
+      name: this.character.literate ? 'De-atomizer' : this.caesarShift('De-atomizer', this.SHIFT_NUM),
+      description: 'Door 2'
     }
   ];
 
   private WIDTH = 400;
   private HEIGHT = 300;
 
-  @ViewChild('layer1')
-  private layer1: ElementRef;
+  // @ViewChild('layer1')
+  // private layer1: ElementRef;
 
-  @ViewChild('layer2')
-  private layer2: ElementRef;
+  // @ViewChild('layer2')
+  // private layer2: ElementRef;
 
-  @ViewChild('layer3')
-  private layer3: ElementRef;
+  // @ViewChild('layer3')
+  // private layer3: ElementRef;
 
   private ctx1: CanvasRenderingContext2D;
   private ctx2: CanvasRenderingContext2D;
@@ -39,17 +58,18 @@ export class GameStepComponent implements OnInit {
 
   ngOnInit() {
 
-    this.background.src ="https://i.ytimg.com/vi/PWtNdTbf-XQ/maxresdefault.jpg";
-    this.foreground.src = "https://html5.litten.com/layers/city.png";
-    this.kid.src = "https://content.mycutegraphics.com/graphics/kids/boy-carrying-football.png";
-    this.ctx1 = (<HTMLCanvasElement>this.layer1.nativeElement).getContext('2d');
-    this.ctx2 = (<HTMLCanvasElement>this.layer2.nativeElement).getContext('2d');
-    this.ctx3 = (<HTMLCanvasElement>this.layer3.nativeElement).getContext('2d');
-    this.drawAll, 20;
+    // this.background.src ="https://i.ytimg.com/vi/PWtNdTbf-XQ/maxresdefault.jpg";
+    // this.foreground.src = "https://html5.litten.com/layers/city.png";
+    // this.kid.src = "https://content.mycutegraphics.com/graphics/kids/boy-carrying-football.png";
+    // this.ctx1 = (<HTMLCanvasElement>this.layer1.nativeElement).getContext('2d');
+    // this.ctx2 = (<HTMLCanvasElement>this.layer2.nativeElement).getContext('2d');
+    // this.ctx3 = (<HTMLCanvasElement>this.layer3.nativeElement).getContext('2d');
+    // this.drawAll, 20;
   }
 
-  public makeChoice() {
-
+  public makeChoice(id: number) {
+    var choice = this.choices.find((v) => v.id === id);
+    console.log('Choice made: ' + JSON.stringify(choice));
   }
 
   private drawAll() {
@@ -79,4 +99,45 @@ export class GameStepComponent implements OnInit {
     }
   }
 
+
+  private caesarShift(str: string, amount: number): string {
+
+    // Wrap the amount
+    if (amount < 0)
+      return this.caesarShift(str, amount + 26);
+
+    // Make an output variable
+    var output = '';
+
+    // Go through each character
+    for (var i = 0; i < str.length; i ++) {
+
+      // Get the character we'll be appending
+      var c = str[i];
+
+      // If it's a letter...
+      if (c.match(/[a-z]/i)) {
+
+        // Get its code
+        var code = str.charCodeAt(i);
+
+        // Uppercase letters
+        if ((code >= 65) && (code <= 90))
+          c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+
+        // Lowercase letters
+        else if ((code >= 97) && (code <= 122))
+          c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+
+      }
+
+      // Append
+      output += c;
+
+    }
+
+    // All done!
+    return output;
+
+  };
 }
