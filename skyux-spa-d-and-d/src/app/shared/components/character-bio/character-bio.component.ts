576d0aead562f5f6cf41835467982e1a3e79a457
Christi Schneider <christineviets@gmail.com>
2019-02-24 13:37:17 -0500

    Localize character bio

--- skyux-spa-d-and-d/src/app/shared/components/character-bio/character-bio.component.ts
+++ skyux-spa-d-and-d/src/app/shared/components/character-bio/character-bio.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { Character } from '../../models/character.model';
 import { SkyAppAssetsService } from '@skyux/assets';
+import { SkyAppResourcesService } from '@skyux/i18n';
 
 @Component({
   selector: 'app-character-bio',
@@ -14,36 +15,52 @@ export class SkyAvatarDemoComponent implements OnInit {
   public character: Character;
 
   public avatarUrl: string;
+  public localizedCountry: string;
+  public localizedGender: string;
 
   constructor(
-    private skyAppAssetsService: SkyAppAssetsService
-  ) { }
+    private skyAppAssetsService: SkyAppAssetsService,
+    private resourcesSvc: SkyAppResourcesService
+  ) {
+
+  }
 
   public ngOnInit() {
-    let ethnicity: string;
+
+    let countryString = 'game_characterbio_country_';
+
+    let pic1: string;
     switch (this.character.country) {
       case 'Zimbabwe':
-        ethnicity = 'Black';
+        pic1 = 'Black';
+        countryString += 'zimbabwe';
         break;
       case 'United States':
         // I know not all Americans are white (and also the other countries their respective ethnicities). TODO roll for skin color.
-        ethnicity = 'Caucasian';
+        pic1 = 'Caucasian';
+        countryString += 'usa';
         break;
       case 'China':
-        ethnicity = 'Asian';
+        pic1 = 'Asian';
+        countryString += 'china';
         break;
       case 'Mexico':
-        ethnicity = 'Hispanic';
+        pic1 = 'Hispanic';
+        countryString += 'mexico';
         break;
       default:
         throw new Error('dev error unknown location 5: ' + location);
     }
-    let gender = this.character.gender === 'female' ? 'Female' : 'Male';
-
+    let pic2 = this.character.gender === 'female' ? 'Female' : 'Male';
     // TODO add hispanic female avatar
-    if (ethnicity !== 'Hispanic' && gender !== 'Female') {
-      this.avatarUrl = this.skyAppAssetsService.getUrl(`avatars/${ethnicity}${gender}.png`);
+    if (pic1 !== 'Hispanic' && pic2 !== 'Female') {
+      this.avatarUrl = this.skyAppAssetsService.getUrl(`avatars/${pic1}${pic2}.png`);
     }
+
+    this.resourcesSvc.getString(countryString)
+      .subscribe((s) => this.localizedCountry = s);
+    this.resourcesSvc.getString('game_characterbio_gender_' + this.character.gender)
+      .subscribe((s) => this.localizedGender = s);
 
   }
 
